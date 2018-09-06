379525cc5fa4b1562fba00fe710d0fa3d996d466
ConceptualActualization <conceptualactualization@gmail.com>
2018-09-06 11:35:21 -0400

    API call to Open Weather App working well with static params

--- express-server/controllers/weatherAPI.js
+++ express-server/controllers/weatherAPI.js
@@ -1,10 +1,24 @@
 const axios = require('axios');
 
+const BASEURL = 'https://api.openweathermap.org'
+
 module.exports = {
-    fiveDayForcast: function(req, res) {
-        console.log('reached');
-        res.json({
-            success:true
+    fiveDayForcast: function (req, res) {
+        console.log(BASEURL + '/data/2.5/forecast?q=London,us&mode=JSON&appid=' + process.env.OPENWEATHERMAP_APPID);
+        axios.get(BASEURL + '/data/2.5/forecast', {
+            params: {
+                q: 'London,us',
+                mode: 'JSON',
+                appid: process.env.OPENWEATHERMAP_APPID
+            }
         })
+            .then(function (response) {
+                console.log(response.data);
+                res.send(response.data);
+            })
+            .catch(function (error) {
+                console.log('error in fiveDayForcast API call: ' + error);
+                res.status(500)
+            })
     }
 }
