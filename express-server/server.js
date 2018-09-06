941c0381a463d329cf098b43249ae098dd899012
ConceptualActualization <conceptualactualization@gmail.com>
2018-09-06 10:24:32 -0400

    (express) API key via dotenv added

--- express-server/server.js
+++ express-server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,5 +11,7 @@ app.get('/api/hello', (req, res) => {
 app.get('/api/forcast', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+console.log(process.env.OPENWEATHERMAP_APPID)
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
