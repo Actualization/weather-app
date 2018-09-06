c65bfac347989fb4f9e8bc91de3aeba6c31b8fe6
ConceptualActualization <conceptualactualization@gmail.com>
2018-09-05 22:53:23 -0400

    beginning of call to weather API

--- express-server/server.js
+++ express-server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
 const app = express();
 const port = process.env.PORT || 5000;
+const weatherAPI = require('./controllers/weatherAPI')
 
+// Routes
 app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.get('/api/forcast', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
